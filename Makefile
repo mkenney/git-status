571b950f8240e7b1e8e9f6e1f59acbaff9f2d7c1
Michael Kenney <michael.kenney@returnpath.com>
2018-04-26 23:26:27 -0600

    save

---
+++ Makefile
@@ -0,0 +1,31 @@
+# Go params
+GOCMD=go
+GOPKG=github.com/mkenney/git-status/pkg
+GOBIN=bin
+GOBUILD=$(GOCMD) build
+GOCLEAN=$(GOCMD) clean
+GOTEST=$(GOCMD) test pkg/...
+GODEP=dep ensure
+BINARY_NAME=git-status
+
+all: clean test build
+build: build-linux build-darwin
+test:
+	$(GOTEST) -v ./...
+clean:
+	$(GOCLEAN)
+	rm -f $(GOBIN)/$(BINARY_NAME)-linux-amd64
+	rm -f $(GOBIN)/$(BINARY_NAME)-darwin-amd64
+run:
+	$(GOBUILD) -o $(BINARY_NAME) -v ./...
+	./$(BINARY_NAME)
+deps:
+	cd $(GOPKG)
+	$(GODEP)
+	cd -
+
+# Cross compilation
+build-linux:
+		CGO_ENABLED=0 GOOS=linux GOARCH=amd64 $(GOBUILD) -o $(GOBIN)/$(BINARY_NAME)-linux-amd64 -v $(GOPKG)
+build-darwin:
+		CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 $(GOBUILD) -o $(GOBIN)/$(BINARY_NAME)-darwin-amd64 -v $(GOPKG)
---
+++ pkg/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+func main() {
+	state := &gitState{data: make(map[string]string)}
+	if len(os.Args) > 1 && "-v" == os.Args[1] {
+		state.verbose = true
+	}
+	state.init()
+	fmt.Printf("\n%s\n", state)
+}
+
+type gitState struct {
+	verbose bool
+
+	// Ref data
+	attached bool
+	data     map[string]string
+	hash     string
+	named    bool
+	refName  string
+	tagged   bool
+	upstream bool
+
+	// local state data
+	added     int
+	ahead     int
+	behind    int
+	deleted   int
+	renamed   int
+	staged    int
+	total     int
+	unstaged  int
+	untracked int
+}
+
+func (state *gitState) init() {
+	state.initRefState()
+	state.initLocalState()
+}
+
+var loadMux sync.Mutex
+
+func (state *gitState) load(commands map[string][]string) {
+	doneCh := make(chan bool)
+	for k, cmd := range commands {
+		fnK := k
+		fnCmd := cmd
+		go func() {
+			out, err := exec.Command("git", fnCmd...).Output()
+			loadMux.Lock()
+			if nil == err {
+				state.data[fnK] = strings.Trim(string(out), "\t\n' ")
+			} else {
+				state.data[fnK] = ""
+			}
+			loadMux.Unlock()
+			doneCh <- true
+		}()
+	}
+	for a := 0; a < len(commands); a++ {
+		<-doneCh
+	}
+}
+
+func (state *gitState) String() string {
+	origin := "origin"
+	position := "master"
+
+	if !state.upstream {
+		origin = "local"
+	}
+	if "" != state.hash {
+		position = string([]rune(state.hash)[:10])
+	}
+	if state.named {
+		position = state.data["branch"]
+	}
+	if !state.attached {
+		origin = "detached"
+	}
+	if state.tagged {
+		origin = "tag"
+		position = state.data["tag"]
+	}
+
+	//fmt.Printf(`
+	//	state.behind: %d, state.ahead: %d, state.deleted: %d, state.added: %d, state.renamed: %d, state.staged: %d, state.unstaged: %d, state.untracked: %d, state.total: %d
+	//`, state.behind, state.ahead, state.deleted, state.added, state.renamed, state.staged, state.unstaged, state.untracked, state.total)
+	status := ""
+	if state.behind > 0 {
+		status = fmt.Sprintf("%s ↓%d", status, state.behind)
+	}
+	if state.ahead > 0 {
+		status = fmt.Sprintf("%s ↑%d", status, state.ahead)
+	}
+	if state.deleted > 0 {
+		status = fmt.Sprintf("%s ✖%d", status, state.deleted)
+	}
+	if state.added > 0 {
+		status = fmt.Sprintf("%s ✚%d", status, state.added)
+	}
+	if state.renamed > 0 {
+		status = fmt.Sprintf("%s ↪%d", status, state.renamed)
+	}
+	if state.staged > 0 {
+		status = fmt.Sprintf("%s ✔%d", status, state.staged)
+	}
+	if state.unstaged > 0 {
+		status = fmt.Sprintf("%s ✎%d", status, state.unstaged)
+	}
+	if state.untracked > 0 {
+		status = fmt.Sprintf("%s …%d", status, state.untracked)
+	}
+	if state.total > 0 {
+		status = fmt.Sprintf("%s #%d", status, state.total)
+	}
+
+	if state.verbose {
+		tmp, _ := json.MarshalIndent(state.data, "", "    ")
+		fmt.Println(string(tmp))
+	}
+
+	return fmt.Sprintf("⎇ %s/%s%s", origin, position, status)
+}
+
+var localStateCommands = map[string][]string{
+	"status": {"status", "--porcelain"},
+	"diff":   {"diff", "--name-only"},
+}
+
+func (state *gitState) initLocalState() {
+	cmpRef := "HEAD"
+	if state.attached {
+		cmpRef = "origin"
+	}
+	localStateCommands["rev-list"] = strings.Split(fmt.Sprintf("rev-list --left-right --count %s...%s", state.hash, cmpRef), " ")
+	state.load(localStateCommands)
+
+	if "" != state.data["rev-list"] {
+		parts := strings.Split(state.data["rev-list"], "\t")
+		state.ahead, _ = strconv.Atoi(parts[0])
+		state.behind, _ = strconv.Atoi(parts[1])
+	}
+
+	status := strings.Split(state.data["status"], "\n")
+	if "" != state.data["diff"] {
+		state.unstaged = len(strings.Split(state.data["diff"], "\n"))
+	}
+	state.total = len(status)
+	for _, stat := range status {
+		runes := []rune(stat)
+		a := string(runes[0])
+		b := string(runes[1])
+		//fmt.Printf("\nStatus: '%s', a: '%s', b: '%s', diff: '%s'\n", stat, a, b, state.data["diff"])
+		if "A" == a || "A" == b {
+			state.added++
+		}
+		if "D" == a || "D" == b {
+			state.deleted++
+		}
+		if "M" == a || "M" == b {
+			state.staged++
+		}
+		if "R" == a || "R" == b {
+			state.renamed++
+		}
+		if "?" == a || "?" == b {
+			state.untracked++
+		}
+	}
+	state.staged -= state.unstaged
+	if state.staged < 0 {
+		state.staged = 0
+	}
+}
+
+var refStateCommands = map[string][]string{
+	"abbrev":   {"rev-parse", "--abbrev-ref", "HEAD"},
+	"branch":   {"symbolic-ref", "--short", "HEAD"},
+	"hash":     {"rev-parse", "HEAD"},
+	"ref":      {"rev-parse", "--symbolic-full-name", "HEAD"},
+	"tag":      {"describe", "--exact-match", "--tags", "HEAD"},
+	"upstream": {"for-each-ref", "--format='%(upstream:short)'", "$(git symbolic-ref -q HEAD 2> /dev/null)"},
+}
+
+func (state *gitState) initRefState() {
+	state.load(refStateCommands)
+	state.hash = state.data["hash"]
+	if "" != state.data["upstream"] || "HEAD" != state.data["abbrev"] || "" != state.data["tag"] {
+		state.attached = true
+	}
+	if "" != state.data["upstream"] {
+		state.upstream = true
+	}
+	if "" != state.data["branch"] {
+		state.named = true
+	}
+	if "" != state.data["tag"] {
+		state.tagged = true
+	}
+}
